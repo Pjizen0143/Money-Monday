fc2f2c4984d9aff57439fedd15ffb8abd3af752e
Pjizen0143 <parabolic0143@gmail.com>
2025-05-14 20:44:11 +0700

    /

--- Frontend/money_monday/lib/main.dart
+++ Frontend/money_monday/lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:money_monday/screen/opening/login_page.dart';
-import 'package:money_monday/utils/app_style.dart';
+import 'package:money_monday/views/auth/login_page.dart';
+import 'package:money_monday/views/utils/app_style.dart';
+
+final GlobalKey<NavigatorState> _navigatorkey = GlobalKey<NavigatorState>();
 
 void main() {
-  MaterialApp app = MaterialApp(
-    title: "My frist app",
-    theme: ThemeData(fontFamily: "OpenSans"),
-    home: Scaffold(body: LogIn(), backgroundColor: AppTheme.orange),
-  );
-  runApp(app);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // กำหนด _navigatorkey ให้กับคุณสมบัติ navigatorKey ของ MaterialApp
+      navigatorKey: _navigatorkey,
+      title: "My first app",
+      theme: ThemeData(fontFamily: "OpenSans"),
+      home: Scaffold(body: LogIn(), backgroundColor: AppTheme.orange),
+    );
+  }
 }
--- Frontend/money_monday/lib/views/utils/widgets/app_style_container.dart
+++ Frontend/money_monday/lib/views/utils/widgets/app_style_container.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:money_monday/utils/app_style.dart';
+import 'package:money_monday/views/utils/app_style.dart';
 
 class AppStyleContainer extends StatelessWidget {
   final double width;
-  final double height;
   final double borderRadius;
   final Widget? child; // เพิ่ม child เพื่อรองรับ widget ลูก
 
   const AppStyleContainer({
     super.key,
     this.width = 691,
-    this.height = 676,
     this.borderRadius = 31,
     this.child, // ให้ child เป็นพารามิเตอร์
   });
@@ -19,7 +17,6 @@ class AppStyleContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: width * 0.63,
-      height: height * 0.63,
       decoration: BoxDecoration(
         color: AppTheme.cream, // ใช้สีจาก AppTheme
         borderRadius: BorderRadius.circular(borderRadius),
