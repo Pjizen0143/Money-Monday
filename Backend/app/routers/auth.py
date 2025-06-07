d63305c374658ecf0d6d3a90c1095c9d02ae33a2
Pjizen0143 <parabolic0143@gmail.com>
2025-06-07 12:56:55 +0700

    update login post model

--- Backend/app/routers/auth.py
+++ Backend/app/routers/auth.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from app.models.user import User, UserUpdate
+from app.models.user import User, UserPublic
 from app.db import SessionDep
 from app.crud.user import verify_password
 from sqlmodel import select
@@ -7,7 +7,7 @@ from sqlmodel import select
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.post("/login")
-def login( session: SessionDep, form_data: UserUpdate):
+def login( session: SessionDep, form_data: UserPublic):
     # หา user จาก username
     user = session.exec(select(User).where(User.username == form_data.username)).first()
     if not user or not verify_password(form_data.password, user.hashed_password):
